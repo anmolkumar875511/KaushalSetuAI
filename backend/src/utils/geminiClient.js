89f6ddc5797c57fce7c2185a2d07766939f9225b
Anmol Kumar <anmolkumar875511@gmail.com>
2026-03-02 13:45:56 +0530

    model(gemini-2.5-flash-lite)

--- backend/src/utils/geminiClient.js
+++ backend/src/utils/geminiClient.js
@@ -3,7 +3,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 export async function queryGemini(prompt) {
-  const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
+  const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash-lite" });
 
   const result = await model.generateContent(prompt);
   console.log('Result: ', result)
