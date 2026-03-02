70dab7ffe10d1cd54c9748b1a8df186c0148e547
Anmol Kumar <anmolkumar875511@gmail.com>
2026-03-02 09:08:53 +0530

    model update and logging added

--- backend/src/utils/geminiClient.js
+++ backend/src/utils/geminiClient.js
@@ -3,10 +3,12 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 export async function queryGemini(prompt) {
-  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash-latest" });
 
   const result = await model.generateContent(prompt);
+  console.log('Result: ', result)
   const response = await result.response;
-
+  console.log('Response: ', response)
+  console.log('Response text: ', response.text())
   return response.text();
 }
