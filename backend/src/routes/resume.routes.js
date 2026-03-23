29b30255b3d42e8b0a574a4ae93d0ae96df66b37
Anmol Kumar <anmolkumar875511@gmail.com>
2026-03-23 09:10:42 +0530

    resumeImprovement route added

--- backend/src/routes/resume.routes.js
+++ backend/src/routes/resume.routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { uploadResume, getLatestResume, updateResume } from '../controllers/resume.controller.js';
+import { uploadResume, getLatestResume, updateResume, getResumeImprovements } from '../controllers/resume.controller.js';
 import { uploadResumeMiddleware } from '../middlewares/upload.middleware.js';
 import { verifyToken } from '../middlewares/auth.middleware.js';
 
@@ -10,5 +10,6 @@ router.use(verifyToken);
 router.get('/', getLatestResume);
 router.post('/upload', uploadResumeMiddleware.single('resume'), uploadResume);
 router.put('/:id', updateResume);
+router.post('/improve', getResumeImprovements);
 
 export default router;
